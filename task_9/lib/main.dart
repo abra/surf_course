d1bfea61bbf7c1f3395db3bf29f3fcf58f4eae21
Aidar Khabibullin <aydar.kh@gmail.com>
2023-08-24 14:07:32 +0500

    Task 9: Исправления

--- task_9/lib/main.dart
+++ task_9/lib/main.dart
@@ -1,11 +1,6 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
-List<Image> catImages = [];
-
 Future<void> main() async {
-  catImages = await _loadCatImages();
   runApp(const MyApp());
 }
 
@@ -34,21 +29,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
-  int _imageIndex = 0;
   final double _widgetHeight = 150;
   final double _widgetWidth = 150;
-  late Image _currentImage;
+  int _imageIndex = 0;
+  late List<String> _imagePathList;
   late final AnimationController _controller;
   late final Animation<double> _animation;
-  late Offset offset;
-  late double _screenWidth;
-  late double _screenHeight;
-  bool _firstStart = true;
+  late Size _screenSize;
+  Offset? offset;
 
   @override
   void initState() {
     super.initState();
-    _currentImage = Image(image: catImages[_imageIndex].image);
+
+    _imagePathList = _loadImagesPathList();
 
     _controller = AnimationController(
       duration: const Duration(seconds: 1),
@@ -67,23 +61,22 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     super.dispose();
   }
 
-  void _changeImage() {
-    if (_imageIndex < catImages.length - 1) {
-      _currentImage = Image(image: catImages[++_imageIndex].image);
+  void _changeImageIndex() {
+    if (_imageIndex < _imagePathList.length - 1) {
+      ++_imageIndex;
     } else {
       _imageIndex = 0;
-      _currentImage = Image(image: catImages[_imageIndex].image);
     }
     setState(() {});
   }
 
   void _onPanUpdate(DragUpdateDetails details) {
-    double x = (offset.dx + details.delta.dx)
-        .clamp(0, _screenWidth - _widgetWidth)
+    double x = (offset!.dx + details.delta.dx)
+        .clamp(0, _screenSize.width - _widgetWidth)
         .toDouble();
 
-    double y = (offset.dy + details.delta.dy)
-        .clamp(0, _screenHeight - _widgetHeight)
+    double y = (offset!.dy + details.delta.dy)
+        .clamp(0, _screenSize.height - _widgetHeight)
         .toDouble();
 
     setState(() {
@@ -91,29 +84,24 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     });
   }
 
-  void _rotate() {
+  void _startRotate() {
     _controller.repeat();
   }
 
-  void _stopRotate(LongPressEndDetails details) {
+  void _stopRotate(_) {
     _controller.reset();
     _controller.stop();
   }
 
-  void _init(BuildContext context) {
-    if (_firstStart) {
-      _screenWidth = MediaQuery.of(context).size.width;
-      _screenHeight = MediaQuery.of(context).size.height;
-      final currentPosX = _screenWidth / 2 - _widgetWidth / 2;
-      final currentPosY = _screenHeight / 2 - _widgetHeight / 2;
-      offset = Offset(currentPosX, currentPosY);
-      _firstStart = false;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    _init(context);
+    _screenSize = MediaQuery.of(context).size;
+
+    if (offset == null) {
+      final centerX = _screenSize.width / 2 - _widgetWidth / 2;
+      final centerY = _screenSize.height / 2 - _widgetHeight / 2;
+      offset = Offset(centerX, centerY);
+    }
 
     return ColoredBox(
       color: const Color(0xFF077777),
@@ -121,14 +109,14 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         children: [
           AnimatedPositioned(
             duration: const Duration(milliseconds: 10),
-            top: offset.dy,
-            left: offset.dx,
+            top: offset!.dy,
+            left: offset!.dx,
             child: RotationTransition(
               turns: _animation,
               child: AnimatedContainer(
                 decoration: BoxDecoration(
                   image: DecorationImage(
-                    image: _currentImage.image,
+                    image: AssetImage(_imagePathList[_imageIndex]),
                     fit: BoxFit.cover,
                   ),
                   borderRadius: BorderRadius.circular(7),
@@ -141,9 +129,9 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           ),
           Positioned.fill(
             child: GestureDetector(
-              onTap: _changeImage,
+              onTap: _changeImageIndex,
               onPanUpdate: _onPanUpdate,
-              onLongPress: _rotate,
+              onLongPress: _startRotate,
               onLongPressEnd: _stopRotate,
             ),
           ),
@@ -153,11 +141,11 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   }
 }
 
-Future<List<Image>> _loadCatImages() async {
-  final images = <Image>[];
+List<String> _loadImagesPathList() {
+  final images = <String>[];
 
   for (int i = 1; i <= 8; i++) {
-    images.add(Image(image: AssetImage('assets/cats/$i.png')));
+    images.add('assets/cats/$i.png');
   }
 
   return images;
